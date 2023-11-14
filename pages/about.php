d8a50d446a0bfbc541daaecdfe50e0bf781ed5a5
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2023-11-14 11:42:18 +0330

    Create about.php

---
+++ pages/about.php
@@ -0,0 +1,398 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  
+    <link rel="apple-touch-icon" sizes="76x76" href="https://s2.uupload.ir/files/studying-student_90ie_xrhl.png">
+  <link rel="icon" type="image/png" href="https://s2.uupload.ir/files/studying-student_90ie_xrhl.png">
+
+  <title>درباره ما</title>
+  <meta content="" name="با تیم ما آشنا شوید">
+  <meta content="" name="سالن‌مطالعه ، درباره ما">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/main.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Logis - v1.3.0
+  * Template URL: https://bootstrapmade.com/logis-bootstrap-logistics-website-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body style="direction: rtl; font-family: B Koodak;">
+
+  <!-- ======= Header ======= -->
+    <?php include("header.php");?>  
+  <!-- End Header -->
+
+  <main id="main" style="font-family: B Koodak">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <div class="breadcrumbs">
+      <div class="page-header d-flex align-items-center" style="background-image: url('assets/img/testimonials-bg.jpg');">
+        <div class="container position-relative">
+          <div class="row d-flex justify-content-center">
+            <div class="col-lg-6 text-center">
+              <h2 style="font-family: B Koodak">درباره ما</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <nav>
+        <div class="container" style="font-family: B Koodak">
+          <ol>
+            <li><a href="index">خانه</a></li>
+            <li>درباره ما</li>
+          </ol>
+        </div>
+      </nav>
+    </div><!-- End Breadcrumbs -->
+
+    <!-- ======= About Us Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row gy-4" style="font-family: B Koodak">
+          <div class="col-lg-6 position-relative align-self-start order-lg-last order-first">
+            <img src="https://s2.uupload.ir/files/studying-student_90ie_xrhl.png" class="img-fluid" alt="">
+            <a href="https://aparat.com/salonmotalee" class="glightbox play-btn"></a>
+          </div>
+          <div class="col-lg-6 content order-last  order-lg-first">
+            <h3 style="font-family: B Koodak">معرفی سیستم هوشمند مدیریت سالن‌مطالعه</h3>
+            <p style="font-family: B Koodak">
+سیستم هوشمند مدیریت سالن مطالعه به منظور ارتقا سطح کیفی سالن‌های مطالعه طراحی شده است. این سیستم علاوه بر پنل‌های هوشمندی که تمامی افراد در ارتباط با سالن‌های مطالعه را شامل می‌شود، به دیوایس‌های هوشمندی همچون دستگاه تشخیص چهره، اثر انگشت، قفل‌های کارتی یا اثر انگشتی و... مجهز می‌باشد. همچنین این سیستم از الگوریتم‌های بسیار کاربری هوش‌مصنوعی و یادگیری ماشین در جهت تجزیه و تحلیل اطلاعات استفاده می‌کند. تعدادی از این پنل‌ها عبارتند از:            </p>
+            <ul style="font-family: B Koodak;">
+              <li><i class="bi bi-check-circle" style="font-size: 25px;"></i> پنل مدیران و اموراجرایی</li>
+              <li><i class="bi bi-check-circle" style="font-size: 25px;"></i> پنل مشاورین</li>
+              <li><i class="bi bi-check-circle" style="font-size: 25px;"></i> پنل مراقبین</li>
+              <li><i class="bi bi-check-circle" style="font-size: 25px;"></i> پنل امور مالی</li>
+              <li><i class="bi bi-check-circle" style="font-size: 25px;"></i> پنل امور انضباطی</li>
+              <li><i class="bi bi-check-circle" style="font-size: 25px;"></i> پنل اولیا و دانش‌آموزان</li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Us Section -->
+
+    <!-- ======= Stats Counter Section ======= --
+    <section id="stats-counter" class="stats-counter pt-0">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row gy-4" style="font-family: B Koodak">
+
+          <div class="col-lg-3 col-md-6">
+            <div class="stats-item text-center w-100 h-100">
+              <span data-purecounter-start="0" data-purecounter-end="3" data-purecounter-duration="1" class="purecounter"></span>
+              <p>شهرها</p>
+            </div>
+          </div><!-- End Stats Item --
+
+          <div class="col-lg-3 col-md-6">
+            <div class="stats-item text-center w-100 h-100">
+              <span data-purecounter-start="0" data-purecounter-end="8" data-purecounter-duration="1" class="purecounter"></span>
+              <p>موسسات</p>
+            </div>
+          </div><!-- End Stats Item -
+
+          <div class="col-lg-3 col-md-6">
+            <div class="stats-item text-center w-100 h-100">
+              <span data-purecounter-start="0" data-purecounter-end="16" data-purecounter-duration="1" class="purecounter"></span>
+              <p>سالن‌ها</p>
+            </div>
+          </div><!-- End Stats Item --
+
+          <div class="col-lg-3 col-md-6">
+            <div class="stats-item text-center w-100 h-100">
+              <span data-purecounter-start="0" data-purecounter-end="1463" data-purecounter-duration="1" class="purecounter"></span>
+              <p>دانش‌آموزان</p>
+            </div>
+          </div><!-- End Stats Item --
+
+        </div>
+
+      </div>
+    </section><!-- End Stats Counter Section -->
+
+    <!-- ======= Our Team Section ======= -->
+    <section id="team" class="team pt-0">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-header">
+          <h2 style="font-family: B Koodak"> تیم ما</h2>
+
+        </div>
+
+        <div class="row" data-aos="fade-up" data-aos-delay="100">
+
+          <div class="col-lg-4 col-md-6 d-flex" style="font-family: B Koodak">
+            <div class="member">
+              <img style="width: 400px;" src="assets/img/team/RN.jpg" class="img-fluid" alt="">
+              <div class="member-content">
+                <h4>مهندس رضا ناظری</h4>
+                <span>مدیر ارشد مارکتینگ </span>
+                <p>
+                    مدیرعامل شرکت فنی‌مهندسی فرداد. کارشناسی ارشد فناوری اطلاعات از دانشگاه فردوسی مشهد
+                </p>
+                <div class="social">
+                  
+                </div>
+              </div>
+            </div>
+          </div><!-- End Team Member -->
+
+          <div class="col-lg-4 col-md-6 d-flex">
+            <div class="member">
+              <img style="width: 400px;" src="assets/img/team/SR.jpg" class="img-fluid" alt="سجاد رنجبر یزدی">
+              <div class="member-content">
+                <h4> سجاد رنجبر یزدی</h4>
+                <span>مدیر ارشد وب و توسعه دهنده بک‌اند</span>
+                <p>
+                     کارشناسی علوم کامپیوتر از دانشگاه فردوسی مشهد
+                </p>
+                <div class="social">
+                  <a href="http://www.linkedin.com/in/sajjad-ranjbar-yazdi-52aaa91a8"><i class="bi bi-linkedin"></i></a>
+                </div>
+              </div>
+            </div>
+          </div><!-- End Team Member -->
+          
+          <div class="col-lg-4 col-md-6 d-flex">
+            <div class="member">
+              <img src="assets/img/team/HG.jpg" class="img-fluid" alt="حمیدرضا گندمی">
+              <div class="member-content">
+                <h4>مهندس حمیدرضا گندمی</h4>
+                <span>توسعه دهنده ارشد بک‌اند و کارشناس نرم‌افزار</span>
+                <p>
+                    کارشناسی علوم کامپیوتر دانشگاه فردوسی مشهد
+                </p>
+                <p>
+                    کارشناسی ارشد علوم کامپیوتر دانشگاه صنعتی شریف
+                </p>
+                <div class="social">
+                  <a href="https://www.linkedin.com/in/hamidreza-gandomi"><i class="bi bi-linkedin"></i></a>
+                </div>
+              </div>
+            </div>
+          </div><!-- End Team Member -->
+
+          <div class="col-lg-4 col-md-6 d-flex">
+            <div class="member">
+              <img style="width: 400px;" src="assets/img/team/EA.jpg" class="img-fluid" alt="">
+              <div class="member-content">
+                <h4>مهندس احسان آریانفر</h4>
+                <span>کارشناس سخت‌افزار و راه‌اندازی سیستم</span>
+                <p>
+                     کارشناسی مهندسی کامپیوتر دانشگاه خراسان
+                </p>
+                <div class="social">
+                  
+                </div>
+              </div>
+            </div>
+          </div><!-- End Team Member -->
+          
+          
+          
+          <div class="col-lg-4 col-md-6 d-flex">
+            <div class="member">
+              <img src="assets/img/team/HN.jpg" class="img-fluid" alt="">
+              <div class="member-content">
+                <h4>مهندس هلیا نازفروش </h4>
+                <span>کارشناس SEO پشتیبان نرم‌افزار و وب‌سایت </span>
+                <p>
+                    کارشناسی مهندسی کامپیوتر دانشگاه خیام
+                </p>
+                <div class="social">
+                  <a href="https://www.linkedin.com/in/heliya-nazforush-412b6b116/"><i class="bi bi-linkedin"></i></a>
+                </div>
+              </div>
+            </div>
+          </div><!-- End Team Member -->
+
+        </div>
+
+      </div>
+    </section><!-- End Our Team Section -->
+
+    <!-- ======= Testimonials Section ======= -->
+    <section id="testimonials" class="testimonials">
+
+      <div class="section-header">
+          <h2 style="color: aliceblue;">نظرات برخی از مشتریان ما</h2>
+
+        </div>
+
+
+      <div class="container">
+
+        
+        
+        <div class="slides-1 swiper" data-aos="fade-up">
+          <div class="swiper-wrapper">
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <img src="https://s2.uupload.ir/files/dsc_9620_2_nmbh.jpg" class="testimonial-img" alt="رسول شجاعی">
+                <h3>مهندس رسول شجاعی</h3>
+                <h4>معاون سالن‌مطالعه دبیرستان استعدادهای درخشان هاشمی‌نژاد۳ مشهد</h4>
+                <div class="stars">
+                  <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
+                </div>
+                <p style="font-family: B Koodak;">
+                  <i class="bi bi-quote quote-icon-left"></i>
+                   در هر جایگاهی که هستید میتوانید از نقاط قوت و ضعف حاضرین در سالن به صورت روزانه، هفتگی و ماهیانه مطلع شوید. به راحتی می‌توان گفت این نرم‌افزار مکمل و حتی در بعضی موارد جایگزین مشاور تحصیلی دانش‌آموزان می‌تواند باشد. 
+                  <i class="bi bi-quote quote-icon-right"></i>
+                </p>
+              </div>
+            </div><!-- End testimonial item -->
+
+
+
+
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+
+      </div>
+    </section><!-- End Testimonials Section -->
+
+    <!-- ======= Frequently Asked Questions Section ======= -->
+    <section id="faq" class="faq">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-header">
+          <span>سوالات متداول</span>
+          <h2>سوالات متداول</h2>
+
+        </div>
+
+        <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="200">
+          <div class="col-lg-10">
+
+            <div class="accordion accordion-flush" id="faqlist" style="font-family: B Koodak; text-align: right;">
+
+              <div class="accordion-item">
+                <h3 class="accordion-header">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-1" style="font-family: B Koodak; text-align: right;">
+                    <i class="bi bi-question-circle question-icon"></i>
+                    با هر پنل چند سالن‌مطالعه را می‌توان مدیریت کرد؟
+                  </button>
+                </h3>
+                <div id="faq-content-1" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                  <div class="accordion-body">
+                    محدویتی در تعداد سالن‌های مطالعه شما وجود ندارد. سیستم هوشمند مدیریت سالن‌مطالعه کاملا پویا طراحی شده و به شما امکان مدیریت تعداد نامحدودی سالن را میدهد.
+                  </div>
+                </div>
+              </div><!-- # Faq item-->
+
+              <div class="accordion-item">
+                <h3 class="accordion-header">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-2">
+                    <i class="bi bi-question-circle question-icon"></i>
+                    آیا بابت تعداد پنل‌های پرسنلی نیز باید هزینه‌ای پرداخت شود؟
+                  </button>
+                </h3>
+                <div id="faq-content-2" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                  <div class="accordion-body">
+                    خیر، هیچ هزینه و محدودیتی درباره تعداد پرسنل شما وجود ندارد.
+                  </div>
+                </div>
+              </div><!-- # Faq item-->
+
+              <div class="accordion-item">
+                <h3 class="accordion-header">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-3">
+                    <i class="bi bi-question-circle question-icon"></i>
+                    آیا اطلاعات مهمی چون کد ملی دانش‌‌آموزان برای استفاده و ثبت‌نام در سیستم نیاز است؟
+                  </button>
+                </h3>
+                <div id="faq-content-3" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                  <div class="accordion-body">
+                    خیر، برای استفاده از سیستم تنها نام و نام خانوادگی نیاز است. اطلاعاتی مانند شماره‌تماس، پایه، رشته و ... در صورت نیاز شما در سیستم ثبت خواهند شد.
+                  </div>
+                </div>
+              </div><!-- # Faq item-->
+
+              <div class="accordion-item">
+                <h3 class="accordion-header">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-4">
+                    <i class="bi bi-question-circle question-icon"></i>
+                    دستگاه‌های اثر انگشت و تجهیزات هوشمند تا چه مدت شامل گارانتی می‌شوند؟
+                  </button>
+                </h3>
+                <div id="faq-content-4" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                  <div class="accordion-body">
+                    <i class="bi bi-question-circle question-icon"></i>
+                    تمامی دستگاه‌ها مشمول ۱۰ سال گارانتی خواهند بود.
+                  </div>
+                </div>
+              </div><!-- # Faq item-->
+
+              <div class="accordion-item">
+                <h3 class="accordion-header">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-5">
+                    <i class="bi bi-question-circle question-icon"></i>
+                    آیا امکان سفارش ماژول‌ها و امکانات سفارشی روی نرم‌افزار وجود دارد؟
+                  </button>
+                </h3>
+                <div id="faq-content-5" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                  <div class="accordion-body">
+                    بله، برنامه‌نویسان ما برای انجام این سفارشات آماده‌اند.
+                  </div>
+                </div>
+              </div><!-- # Faq item-->
+
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Frequently Asked Questions Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <?php include("footer.php");?>  
+  <!-- End Footer -->
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
